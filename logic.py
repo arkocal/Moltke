78eefb1ebf506de62d98b6038f3252851ca75512
Maximilian von Tschirschnitz <maximilian.tschirschnitz@gmx.de>
2020-06-19 18:53:28 +0200

    haaaaands
    Signed-off-by: Maximilian von Tschirschnitz <maximilian.tschirschnitz@gmx.de>

--- logic.py
+++ logic.py
@@ -1,8 +1,34 @@
-import pygame
 
-class Player:
-    def __init__(self, position, movement):
+class GameObject:
+    def __init__(self, position, box):
         self.position = position
+        self.box = box
+
+    @property
+    def top_left(self):
+        return self.position - (0.5 * self.box)
+
+    @property
+    def bottom_right(self):
+        return self.position + (0.5 * self.box)
+
+    def is_colliding(self, other):
+        me_tl = self.top_left
+        me_br = self.bottom_right
+        he_tl = other.top_left
+        he_br = other.bottom_right
+        
+        if(me_tl.x < he_tl.x)
+            return False
+        if(me_br.x > he_tl.x)
+            return False
+        if(me_tl.y < he_tl.y)
+            return False
+        if(me_br.y > he_tl.y)
+            return False
+    
+class Player(GameObject):
+    def __init__(self, position, movement):
         self.movement = movement
 
     def turn_cw(self):
@@ -11,9 +37,11 @@ class Player:
     def turn_ccw(self):
         self.movement.rotate(-90)
 
+class Obstacle(GameObject):
+    pass
+
 class Game:
     def __init__(self):
-        self.player_count = 2
         self.players = []
 
     def add_player(self, player):
